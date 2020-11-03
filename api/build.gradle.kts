94b6a2b9c9c8b347d2c578d364801c30df39fd10
Egor Shakhmin <lapter57@gmail.com>
2020-11-04 02:25:59 +0300

    Fixed configuration of api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,29 +1,39 @@
 plugins {
-    id("org.springframework.boot") version "2.3.4.RELEASE"
+    id("org.springframework.boot") version "2.3.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
 }
 
 version = "1.0-SNAPSHOT"
 
+extra["cassandra-driver.version"] = "4.9.0"
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.jetbrains:annotations:19.0.0")
-
-    // Driver (overriding spring defaults)
-    implementation("com.datastax.oss:java-driver-core:4.9.0")
-    implementation("com.datastax.oss:java-driver-query-builder:4.9.0")
-    implementation("com.datastax.oss:java-driver-mapper-runtime:4.9.0")
-    implementation("com.datastax.oss:native-protocol:1.4.11")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
-    testImplementation("org.springframework.security:spring-security-test")
-
-    val lombok = "org.projectlombok:lombok:1.18.16"
+    // Lombok
+    val lombok = "org.projectlombok:lombok"
     compileOnly(lombok)
     annotationProcessor(lombok)
     testCompileOnly(lombok)
     testAnnotationProcessor(lombok)
+
+    // Driver (overriding spring defaults)
+    annotationProcessor("com.datastax.oss:java-driver-mapper-processor")
+    testAnnotationProcessor("com.datastax.oss:java-driver-mapper-processor")
+
+    implementation("com.datastax.oss:java-driver-core")
+    implementation("com.datastax.oss:java-driver-query-builder")
+    implementation("com.datastax.oss:java-driver-mapper-runtime")
+    implementation("com.datastax.oss:native-protocol")
+
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // Annotations for better code documentation
+    implementation("org.jetbrains:annotations:19.0.0")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("org.springframework.security:spring-security-test")
 }
