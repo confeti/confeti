d6b0cc925599d71d61abdc794ec6ccad9103fb99
Egor Shakhmin <lapter57@gmail.com>
2020-11-12 01:23:06 +0300

    location field in contact_info udt is now text

--- api/src/main/java/org/confeti/db/mapper/BaseMapper.java
+++ api/src/main/java/org/confeti/db/mapper/BaseMapper.java
@@ -17,7 +17,7 @@ import static org.confeti.db.model.speaker.SpeakerEntity.SPEAKER_ATT_NAME;
 import static org.confeti.db.model.udt.ConferenceShortInfoUDT.CONFERENCE_SHORT_INFO_UDT;
 import static org.confeti.db.model.udt.ContactInfoUDT.CONTACT_INFO_ATT_COMPANIES;
 import static org.confeti.db.model.udt.ContactInfoUDT.CONTACT_INFO_ATT_EMAIL;
-import static org.confeti.db.model.udt.ContactInfoUDT.CONTACT_INFO_ATT_LOCATIONS;
+import static org.confeti.db.model.udt.ContactInfoUDT.CONTACT_INFO_ATT_LOCATION;
 import static org.confeti.db.model.udt.ContactInfoUDT.CONTACT_INFO_ATT_TWITTER_USERNAME;
 import static org.confeti.db.model.udt.ContactInfoUDT.CONTACT_INFO_UDT;
 import static org.confeti.db.model.udt.ReportSourceUDT.REPORT_SOURCE_ATT_PRESENTATION_URL;
@@ -29,29 +29,9 @@ import static org.confeti.db.model.udt.SpeakerCompanyUDT.SPEAKER_COMPANY_ATT_NAM
 import static org.confeti.db.model.udt.SpeakerCompanyUDT.SPEAKER_COMPANY_ATT_YEAR;
 import static org.confeti.db.model.udt.SpeakerCompanyUDT.SPEAKER_COMPANY_UDT;
 import static org.confeti.db.model.udt.SpeakerFullInfoUDT.SPEAKER_FULL_INFO_UDT;
-import static org.confeti.db.model.udt.SpeakerLocationUDT.SPEAKER_LOCATION_ATT_ADDED_DATE;
-import static org.confeti.db.model.udt.SpeakerLocationUDT.SPEAKER_LOCATION_ATT_NAME;
-import static org.confeti.db.model.udt.SpeakerLocationUDT.SPEAKER_LOCATION_UDT;
 import static org.confeti.db.model.udt.SpeakerShortInfoUDT.SPEAKER_SHORT_INFO_UDT;
 
 public interface BaseMapper {
-
-    /**
-     * Create the <i>speaker_location</i> UDT.
-     *
-     * <pre>
-     * CREATE TYPE IF NOT EXISTS speaker_location (
-     *     added_date timestamp,
-     *     name text
-     * );
-     * </pre>
-     */
-    default void createSpeakerLocationUDT(@NotNull final CqlSession cqlSession) {
-        cqlSession.execute(createType(SPEAKER_LOCATION_UDT).ifNotExists()
-                .withField(SPEAKER_LOCATION_ATT_ADDED_DATE, DataTypes.TIMESTAMP)
-                .withField(SPEAKER_LOCATION_ATT_NAME, DataTypes.TEXT)
-                .build());
-    }
 
     /**
      * Create the <i>speaker_company</i> UDT.
@@ -117,18 +97,17 @@ public interface BaseMapper {
      * CREATE TYPE IF NOT EXISTS contact_info (
      *     companies set&lt;frozen&lt;speaker_company&gt;&gt;,
      *     email text,
-     *     locations set&lt;frozen&lt;speaker_location&gt;&gt;,
+     *     location text,
      *     twitter_username text
      * );
      * </pre>
      */
     default void createContactInfoUDT(@NotNull final CqlSession cqlSession) {
         createSpeakerCompanyUDT(cqlSession);
-        createSpeakerLocationUDT(cqlSession);
         cqlSession.execute(createType(CONTACT_INFO_UDT).ifNotExists()
                 .withField(CONTACT_INFO_ATT_COMPANIES, DataTypes.setOf(udt(SPEAKER_COMPANY_UDT, true)))
                 .withField(CONTACT_INFO_ATT_EMAIL, DataTypes.TEXT)
-                .withField(CONTACT_INFO_ATT_LOCATIONS, DataTypes.setOf(udt(SPEAKER_LOCATION_UDT, true)))
+                .withField(CONTACT_INFO_ATT_LOCATION, DataTypes.TEXT)
                 .withField(CONTACT_INFO_ATT_TWITTER_USERNAME, DataTypes.TEXT)
                 .build());
     }
--- api/src/main/java/org/confeti/db/mapper/speaker/SpeakerByConferenceDaoMapper.java
+++ api/src/main/java/org/confeti/db/mapper/speaker/SpeakerByConferenceDaoMapper.java
@@ -12,15 +12,13 @@ import org.confeti.db.mapper.BaseMapper;
 import org.jetbrains.annotations.NotNull;
 
 import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createTable;
-import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.udt;
 import static org.confeti.db.model.speaker.AbstractSpeakerEntity.SPEAKER_ATT_AVATAR;
 import static org.confeti.db.model.speaker.AbstractSpeakerEntity.SPEAKER_ATT_ID;
 import static org.confeti.db.model.speaker.AbstractSpeakerEntity.SPEAKER_ATT_NAME;
 import static org.confeti.db.model.speaker.SpeakerByConferenceEntity.SPEAKER_BY_CONFERENCE_ATT_CONFERENCE_NAME;
-import static org.confeti.db.model.speaker.SpeakerByConferenceEntity.SPEAKER_BY_CONFERENCE_ATT_LOCATIONS;
+import static org.confeti.db.model.speaker.SpeakerByConferenceEntity.SPEAKER_BY_CONFERENCE_ATT_LOCATION;
 import static org.confeti.db.model.speaker.SpeakerByConferenceEntity.SPEAKER_BY_CONFERENCE_ATT_YEAR;
 import static org.confeti.db.model.speaker.SpeakerByConferenceEntity.SPEAKER_BY_CONFERENCE_TABLE;
-import static org.confeti.db.model.udt.SpeakerLocationUDT.SPEAKER_LOCATION_UDT;
 
 @Mapper
 public interface SpeakerByConferenceDaoMapper extends BaseMapper {
@@ -37,20 +35,19 @@ public interface SpeakerByConferenceDaoMapper extends BaseMapper {
      *     year int,
      *     name text,
      *     avatar text,
-     *     locations set&lt;frozen&lt;speaker_location&gt;&gt;,
+     *     location text,
      *     id uuid,
      *     PRIMARY KEY (conference_name, year, name)
      * ) WITH CLUSTERING ORDER BY (year DESC, name ASC);
      * </pre>
      */
     default void createSchema(@NotNull final CqlSession cqlSession) {
-        createSpeakerLocationUDT(cqlSession);
         cqlSession.execute(createTable(SPEAKER_BY_CONFERENCE_TABLE).ifNotExists()
                 .withPartitionKey(SPEAKER_BY_CONFERENCE_ATT_CONFERENCE_NAME, DataTypes.TEXT)
                 .withClusteringColumn(SPEAKER_BY_CONFERENCE_ATT_YEAR, DataTypes.INT)
                 .withClusteringColumn(SPEAKER_ATT_NAME, DataTypes.TEXT)
                 .withColumn(SPEAKER_ATT_AVATAR, DataTypes.TEXT)
-                .withColumn(SPEAKER_BY_CONFERENCE_ATT_LOCATIONS, DataTypes.setOf(udt(SPEAKER_LOCATION_UDT, true)))
+                .withColumn(SPEAKER_BY_CONFERENCE_ATT_LOCATION, DataTypes.TEXT)
                 .withColumn(SPEAKER_ATT_ID, DataTypes.UUID)
                 .withClusteringOrder(SPEAKER_BY_CONFERENCE_ATT_YEAR, ClusteringOrder.DESC)
                 .withClusteringOrder(SPEAKER_ATT_NAME, ClusteringOrder.ASC)
