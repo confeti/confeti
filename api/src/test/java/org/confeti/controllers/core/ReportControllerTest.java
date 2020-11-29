686c7e86dd3c886a27f122b025a11fc02509c0fd
Andrew Petrov <marsofandrew@gmail.com>
2020-11-29 11:13:56 +0300

    [#24] Implement endpoint for get info about languages (#30)

--- api/src/test/java/org/confeti/controllers/core/ReportControllerTest.java
+++ api/src/test/java/org/confeti/controllers/core/ReportControllerTest.java
@@ -1,7 +1,7 @@
 package org.confeti.controllers.core;
 
 import org.confeti.config.SecurityConfig;
-import org.confeti.controllers.dto.core.TagResponse;
+import org.confeti.controllers.dto.core.ReportResponse;
 import org.confeti.service.ReportService;
 import org.confeti.service.dto.Conference;
 import org.confeti.service.dto.Report;
@@ -36,7 +36,7 @@ public class ReportControllerTest {
     public void testGetReportTagRequestWithYearAndConferenceName() {
         final String conferenceName = "bla";
         final int year = 1970;
-        final TagResponse response = new TagResponse(conferenceName, Map.of(year, Map.of("a", 2L, "b", 1L, "c", 1L)));
+        final ReportResponse response = new ReportResponse(conferenceName, Map.of(year, Map.of("a", 2L, "b", 1L, "c", 1L)));
 
         when(reportService.findBy(conferenceName, year))
                 .thenReturn(Flux.fromIterable(Arrays.asList(
@@ -47,15 +47,9 @@ public class ReportControllerTest {
                                 .tags(Set.of("a", "c"))
                                 .build()
                 )));
-
-        WebTestClient
-                .bindToController(reportController)
-                .build()
-                .get()
-                .uri(String.format("/api/rest/report/tag?year=%d&conference_name=%s", year, conferenceName))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(TagResponse.class).isEqualTo(response);
+        testGetReportInfoRequestWithConferenceName(
+                String.format("/api/rest/report/tag?year=%d&conference_name=%s", year, conferenceName),
+                response);
     }
 
     @Test
@@ -63,7 +57,8 @@ public class ReportControllerTest {
         final Conference conference1 = Conference.builder("test", 1971).build();
         final Conference conference2 = Conference.builder("test", 1972).build();
 
-        final TagResponse response = new TagResponse(conference1.getName(), Map.of(1971, Map.of("a", 2L, "b", 1L, "c", 1L),
+        final ReportResponse response = new ReportResponse(conference1.getName(), Map.of(
+                1971, Map.of("a", 2L, "b", 1L, "c", 1L),
                 1972, Map.of("a", 1L, "c", 1L)));
 
         when(reportService.findBy(conference1.getName()))
@@ -81,15 +76,7 @@ public class ReportControllerTest {
                                 .conferences(Set.of(conference2))
                                 .build()
                 )));
-
-        WebTestClient
-                .bindToController(reportController)
-                .build()
-                .get()
-                .uri(String.format("/api/rest/report/tag?conference_name=%s", conference1.getName()))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(TagResponse.class).isEqualTo(response);
+        testGetReportInfoRequestWithConferenceName(String.format("/api/rest/report/tag?conference_name=%s", conference1.getName()), response);
     }
 
     @Test
@@ -111,18 +98,10 @@ public class ReportControllerTest {
                                 .build(),
                         Report.builder("title2")
                                 .tags(Set.of("a", "c"))
-                                .conferences(Set.of(conference2, conference3))
+                                .conferences(Set.of(conference2))
                                 .build()
                 )));
-
-        WebTestClient
-                .bindToController(reportController)
-                .build()
-                .get()
-                .uri(String.format("/api/rest/report/tag?year=%d", conferenceYear))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(List.class);
+        testGetReportInfoRequest(String.format("/api/rest/report/tag?year=%d", conferenceYear));
     }
 
     @Test
@@ -144,17 +123,151 @@ public class ReportControllerTest {
                                 .build(),
                         Report.builder("title2")
                                 .tags(Set.of("a", "c"))
-                                .conferences(Set.of(conference2, conference3))
+                                .conferences(Set.of(conference2))
+                                .build(),
+                        Report.builder("title3")
+                                .tags(Set.of("a", "c"))
+                                .conferences(Set.of(conference3))
                                 .build()
                 )));
 
+        testGetReportInfoRequest("/api/rest/report/tag");
+    }
+
+    @Test
+    public void testGetReportLanguageRequestWithYearAndConferenceName() {
+        final String conferenceName = "bla";
+        final int year = 1970;
+        final ReportResponse response = new ReportResponse(conferenceName, Map.of(year, Map.of("RU", 3L, "EN", 1L, "FR", 1L)));
+
+        when(reportService.findBy(conferenceName, year))
+                .thenReturn(Flux.fromIterable(Arrays.asList(
+                        Report.builder("title")
+                                .language("RU")
+                                .build(),
+                        Report.builder("title2")
+                                .language("RU")
+                                .build(),
+                        Report.builder("title3")
+                                .language("EN")
+                                .build(),
+                        Report.builder("title4")
+                                .language("RU")
+                                .build(),
+                        Report.builder("title5")
+                                .language("FR")
+                                .build()
+                )));
+        testGetReportInfoRequestWithConferenceName(
+                String.format("/api/rest/report/language?year=%d&conference_name=%s", year, conferenceName),
+                response);
+    }
+
+    @Test
+    public void testGetReportLanguageRequestWithConferenceName() {
+        final Conference conference1 = Conference.builder("test", 1971).build();
+        final Conference conference2 = Conference.builder("test", 1972).build();
+
+        final ReportResponse response = new ReportResponse(conference1.getName(), Map.of(1971, Map.of("RU", 2L),
+                1972, Map.of("EN", 1L, "RU", 1L)));
+
+        when(reportService.findBy(conference1.getName()))
+                .thenReturn(Flux.fromIterable(Arrays.asList(
+                        Report.builder("title")
+                                .language("RU")
+                                .conferences(Set.of(conference1))
+                                .build(),
+                        Report.builder("title11")
+                                .language("RU")
+                                .conferences(Set.of(conference1))
+                                .build(),
+                        Report.builder("title2")
+                                .language("EN")
+                                .conferences(Set.of(conference2))
+                                .build(),
+                        Report.builder("title22")
+                                .language("RU")
+                                .conferences(Set.of(conference2))
+                                .build()
+                )));
+        testGetReportInfoRequestWithConferenceName(String.format("/api/rest/report/language?conference_name=%s", conference1.getName()), response);
+    }
+
+    @Test
+    public void testGetReportLanguageRequestWithYear() {
+        final int conferenceYear = 1971;
+        final Conference conference1 = Conference.builder("test", conferenceYear).build();
+        final Conference conference2 = Conference.builder("test", 1972).build();
+        final Conference conference3 = Conference.builder("test2", conferenceYear).build();
+
+        when(reportService.findAll())
+                .thenReturn(Flux.fromIterable(Arrays.asList(
+                        Report.builder("title")
+                                .language("RU")
+                                .conferences(Set.of(conference1))
+                                .build(),
+                        Report.builder("title11")
+                                .language("Ru-ru")
+                                .conferences(Set.of(conference1))
+                                .build(),
+                        Report.builder("title3")
+                                .language("EN")
+                                .conferences(Set.of(conference3))
+                                .build()
+                )));
+        testGetReportInfoRequest(String.format("/api/rest/report/language?year=%d", conferenceYear));
+    }
+
+    @Test
+    public void testGetReportLanguageRequest() {
+        final int conferenceYear = 1971;
+        final Conference conference1 = Conference.builder("test", conferenceYear).build();
+        final Conference conference2 = Conference.builder("test", 1972).build();
+        final Conference conference3 = Conference.builder("test2", conferenceYear).build();
+
+        when(reportService.findAll())
+                .thenReturn(Flux.fromIterable(Arrays.asList(
+                        Report.builder("title")
+                                .language("RU")
+                                .conferences(Set.of(conference1))
+                                .build(),
+                        Report.builder("title11")
+                                .language("RU")
+                                .conferences(Set.of(conference1))
+                                .build(),
+                        Report.builder("title2")
+                                .language("EN")
+                                .conferences(Set.of(conference2))
+                                .build(),
+                        Report.builder("title2")
+                                .language("EN")
+                                .conferences(Set.of(conference3))
+                                .build()
+                )));
+
+        testGetReportInfoRequest("/api/rest/report/language");
+    }
+
+    private void testGetReportInfoRequestWithConferenceName(final String uri, final ReportResponse expectedResponse) {
         WebTestClient
                 .bindToController(reportController)
                 .build()
                 .get()
-                .uri("/api/rest/report/tag")
+                .uri(uri)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(ReportResponse.class).isEqualTo(expectedResponse);
+    }
+
+    private void testGetReportInfoRequest(final String uri) {
+        WebTestClient
+                .bindToController(reportController)
+                .build()
+                .get()
+                .uri(uri)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(List.class);
     }
+
 }
