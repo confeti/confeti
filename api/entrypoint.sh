c9ac65305a44768b85bb30174847e863687fc5f2
Egor Shakhmin <lapter57@gmail.com>
2020-12-04 16:10:48 +0300

    [#33] Publishing docker images (#35)
    * Added github workflow to publish docker images
    
    * Fixed publishig workflow file
    
    * Fixed building of api container
    
    * Fixed publishing workflow

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-# Set credentials and an Astra DB password
-source setup.sh -c='' -p= && java --enable-preview -jar /confeti.jar
+if [ -z $CONFETI_LOCAL_DB ] || [ $CONFETI_LOCAL_DB = true ]; then
+    java --enable-preview -jar /confeti.jar
+else
+    if [ -z $ASTRA_CREDS ] || [ -z $ASTRA_PASSWD ]; then
+        echo "Credentials and password not set"
+        exit 1
+    else
+        source ./setup.sh -c=$ASTRA_CREDS -p=$ASTRA_PASSWD && java --enable-preview -jar /confeti.jar
+    fi
+fi
